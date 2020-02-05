ed76c4ecfbd7a6040a642137a369111a32554a27
alukardua <prizrack30@gmail.com>
2020-02-05 22:04:06 +0300

    Add copyright 05/02/2020

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -29,7 +29,7 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'old_password'     => 'sometimes|nullable|between:8,15',
+            'old_password'     => 'sometimes|nullable|between:8,15|regex:/[a-zA-Z0-9_]+/',
             'new_password'     => 'same:confirm_password|different:old_password|regex:/[a-zA-Z0-9_]+/|sometimes|nullable|between:8,15',
             'confirm_password' => 'same:new_password|different:old_password|regex:/[a-zA-Z0-9_]+/|sometimes|nullable|between:8,15',
         ];
@@ -41,6 +41,7 @@ class UserRequest extends FormRequest
     public function messages()
     {
         return [
+            'old_password.regex'         => 'Недопустимый формат нового пароля..',
             'old_password.between'       => 'Старый пароль должен содержать от :min до :max символов..',
             'new_password.regex'         => 'Недопустимый формат нового пароля..',
             'new_password.same'          => 'Новый пароль и пароль подтверждения должны совпадать..',
