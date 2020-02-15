9e5d138b560fc96ee2b28f9780a7d018f563f438
alukardua <prizrack30@gmail.com>
2020-02-15 13:30:39 +0300

    Create model and controller studios 15.02.2020

---
+++ app/Models/Studio.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Copyright (c) by anime-free
+ * Date: 2020.
+ * User: Alukardua
+ */
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Studio extends Model
+{
+    //
+}
