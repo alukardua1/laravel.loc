3de259c1cdf869dadfa03241f47b7642be633599
alukardua <prizrack30@gmail.com>
2020-02-16 19:29:28 +0300

    Commit VoteController update 16.02.2020

---
+++ app/Http/Controllers/Main/VoteController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright (c) by anime-free
+ * Date: 2020.
+ * User: Alukardua
+ */
+
+namespace App\Http\Controllers\Main;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\Interfaces\VoteRepositoryInterface;
+use Illuminate\Http\RedirectResponse;
+
+class VoteController extends Controller
+{
+    private static $voteRepository;
+
+   public function __construct(VoteRepositoryInterface $repository)
+   {
+       parent::__construct();
+       self::$voteRepository = $repository;
+   }
+
+    public function plusVotes($id): RedirectResponse
+    {
+        self::$voteRepository->plusVotes($id);
+
+        return back();
+    }
+
+    public function minusVotes($id): RedirectResponse
+    {
+        self::$voteRepository->minusVotes($id);
+
+        return back();
+    }
+}
