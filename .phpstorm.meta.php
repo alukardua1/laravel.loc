9c51da086796b663eb54846a9fc794d2c0dd9c27
alukardua <prizrack30@gmail.com>
2020-07-19 20:21:27 +0300

    Update composer 19.07.2020

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -11,7 +11,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-07-15 03:20:09.
+    * Generated on 2020-07-19 17:19:32.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
